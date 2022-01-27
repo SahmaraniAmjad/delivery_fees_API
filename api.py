4aad0492072534703e26c0bcfefd4046f1117b3b
Amjad <44136023+SahmaraniAmjad@users.noreply.github.com>
2022-01-27 17:28:14 +0100

    third commit

--- api.py
+++ api.py
@@ -11,56 +11,54 @@ class Adder(Resource):
         args = request.args
         print(args)  # For debugging
 
-        # params eval
+        #params eval
         cart_value = args['cart_value']
         delivery_distance = args['delivery_distance']
         number_of_items = args['number_of_items']
         time_for_order = args['time']
-        print(cart_value)
-        print(delivery_distance)
-        print(number_of_items)
-        print(time_for_order)
+        print(type(cart_value))
+        print(type(delivery_distance))
+        print(type(number_of_items))
+        print(type(time_for_order))
 
-        if delivery_distance is None or cart_value is None or number_of_items is None or time_for_order is None:
+        if len(delivery_distance) == 0 or len(cart_value) == 0 or len(number_of_items) == 0 or len(time_for_order) == 0:
+            print(args.get("delivery_distance"))
             return jsonify("Invalid Parameter"), abort(400)
+
         else:
+
             cart_value = float(args['cart_value'])
             delivery_distance = int(args['delivery_distance'])
             number_of_items = int(args['number_of_items'])
             time_for_order = str(args['time'])
 
-        print("cart value is", cart_value)
-        print("delivery distance is", delivery_distance)
-        print("number of items are", number_of_items)
-        print("time is", time_for_order)
+            # if args.get("delivery_distance") is None or args.get("cart_value") is None or args.get(
+            #         "number_of_items") is None or args.get("time_for_order") is None:
+            #     return jsonify("Invalid Parameter"), abort(400)
 
-        # we are calling here class fees
-        db_utils = fees(cart_value, delivery_distance, number_of_items, time_for_order)
-        # we are calling the fees method to calculate the delivery_fees
-        cursor = db_utils.delivery_fees(cart_value, delivery_distance, number_of_items, time_for_order) # without surchage
-        print(cursor)
-        # get day surcharge either 1 or 1.1
+            print("cart value is", cart_value)
+            print("delivery distance is", delivery_distance)
+            print("number of items are", number_of_items)
+            print("time is", time_for_order)
 
-        if cursor > 1500:
-            return jsonify(f"delivery_fee: {1500}")
-        else:
-            return jsonify(f"delivery_fee: {cursor}")
+            # we are calling here class fees
+            db_utils = fees(cart_value, delivery_distance, number_of_items, time_for_order)
+            # we are calling the fees method to calculate the delivery_fees
+            cursor = db_utils.delivery_fees(cart_value, delivery_distance, number_of_items,
+                                            time_for_order)  # without surchage
+            print(cursor)
+            # get day surcharge either 1 or 1.1
 
-        '''cart_value = args.get("cart_value")
-        delivery_distance = args.get("delivery_distance")
-        number_of_items = args.get("number_of_items")
-        time = args.get("time")
-
-        if delivery_distance is None or cart_value is None or number_of_items is None or time is None:
-            return jsonify("Invalid Parameter"), abort(400)
-        else: #cursor == 40
-            #cursor >= 15 cursor = 15 '''
-
-
+            if cursor > 1500:
+                return jsonify(f"delivery_fee: {1500}")
+            elif delivery_distance <= 0 or cart_value <= 0 or number_of_items <= 0 or len(time_for_order) == 0:
+                print(args.get("delivery_distance"))
+                return jsonify("Invalid Parameter"), abort(400)
+            else:
+                return jsonify(f"delivery_fee: {cursor}")
 
 
 api.add_resource(Adder, '/fees')
-
 
 if __name__ == '__main__':
     app.run()
--- fees.py
+++ fees.py
@@ -6,6 +6,12 @@ class fees:
         self.number_of_items = number_of_items
         self.time_for_order = time_for_order
 
+        '''#%Y%m%dT%H%M%S.%fZ
+                  #%Y-%m-%dT%H:%M:%S%Z
+
+                  elif time_for_order.format() != '%Y-%m-%d':
+                      return jsonify("Invalid Parameter"), abort(400)'''
+
     # create a separate functions to calculate the fees for each condition
 
     def fees_for_distance(self):
@@ -29,9 +35,7 @@ class fees:
 
     def fees_for_number_of_items(self, number_of_items):
         self.number_of_items = number_of_items
-        if number_of_items == 0:
-            print("You have to add an item to proceed")
-        elif 1 <= number_of_items <= 4:
+        if 1 <= number_of_items <= 4:
             return 0
         else:
             return (number_of_items - 4) * 50
@@ -42,22 +46,38 @@ class fees:
         # check the hour
         # ISO string  => convert to unix
         print(f"time_for_order_1: {time_for_order}")
-        unix_timestamp = dp.parse(time_for_order)
-        hour = unix_timestamp.hour
-        day = date.today().weekday()
-        print(hour)
-        print(f"unix_timestamp: {unix_timestamp}\n day: {day} \n hour: {hour}")
-        if day == 4 and 15 <= hour <= 19:
-            return 1.1
-        else:
-            print(1)
-            return 1
+        print(f"format of time_for_order: {format(time_for_order)}")
+        f = "%Y-%m-%dT%H:%M:%S"
+        try:
+            datetime.strptime(time_for_order, f)
+            unix_timestamp = dp.parse(time_for_order)
+
+            print(f"Unix_timestamp iso format: {unix_timestamp.isoformat()}")
+
+            print(f"Unix_timestamp iso format: {unix_timestamp.isoformat()}")
+            print(f"time_for_order format:{time_for_order.format()}")
+            hours = unix_timestamp.hour
+            minutes = unix_timestamp.minute
+            seconds = unix_timestamp.second
+            day = date.today().weekday()
+
+            print(f"unix_timestamp: {unix_timestamp}\n day: {day} \n hour: {hours} \n minutes: {minutes} \n seconds: {seconds}")
+            if day == 4 and 15 <= hours <= 18 and 00 <= minutes <= 59 and 00 <= seconds <= 59:
+                return 1.1
+            elif day == 4 and hours == 19 and minutes == 00 and seconds == 00:
+                return 1.1
+            else:
+                return 1
+        except ValueError:
+            print("I am here")
+            return False
+
 
     def delivery_fees(self, cart_value, delivery_distance, number_of_items, time_for_order):
         distance_delivery_fees = self.fees_for_distance()
         items_delivery_fees = self.fees_for_number_of_items(number_of_items)
-        friday_delivery_fees = self.fees_for_friday_rush_hours(time_for_order)  # 1.1 or 1
-        print(f"friday_delivery_fees :{friday_delivery_fees}")
+        hours_delivery_fees = self.fees_for_friday_rush_hours(time_for_order)  # 1.1 or 1
+        print(f"friday_delivery_fees :{hours_delivery_fees}")
 
         surcharge = int(1000 - cart_value)
         fees_with_surcharge = distance_delivery_fees + items_delivery_fees + surcharge
@@ -67,31 +87,10 @@ class fees:
         if cart_value >= 10000:
             return 0
 
-        # 100 cents is 1 Euro, 999 cents is 9.99 Euros
-        # elif 100 <= cart_value <= 999:
-        #     print("Case one")
-        #     if fees_with_surcharge > 1500:
-        #         return "delivery fees are maximum 15 Euros, maybe the distance is too long, or you add too much items"
-        #     else:
-        #         return int(fees_with_surcharge * friday_delivery_fees)
+        elif cart_value < 1000:
+            print("Case one")
+            return int(fees_with_surcharge * hours_delivery_fees)
 
-        elif 100 <= cart_value <= 999:
-            print("Case two")
-            if fees_with_surcharge > 1500:
-                return "delivery fees are maximum 15 Euros, maybe the distance is too long, or you add too much items"
-            else:
-                return int(fees_with_surcharge * friday_delivery_fees)
-
-        # elif cart_value >= 1000:
-        #     print("Case three")
-        #     if fees_without_surcharge * 1.1 > 1500:
-        #         return "delivery fees are maximum 15 Euros, maybe the distance is too long, or you add too much items"
-        #     else:
-        #         return int(fees_without_surcharge * friday_delivery_fees)
         elif cart_value >= 1000:
-            print("Case four")
-            # if fees_without_surcharge > 1500:
-            #     return "delivery fees are maximum 15 Euros, maybe the distance is too long, or you add too much items"
-            # else:
-            #     return int(fees_without_surcharge)
-            return int(fees_without_surcharge * friday_delivery_fees)
+            print("Case tow")
+            return int(fees_without_surcharge * hours_delivery_fees)
